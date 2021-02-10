1a17236d0b135af8c9581dbbb7e56e56739af63f
sathishcharykotha <ksathish@stratapps.com>
2021-02-10 11:21:47 +0530

    Update StringDemo7.java

--- String programs/StringDemo7.java
+++ String programs/StringDemo7.java
@@ -3,7 +3,7 @@ public static void main(String [] args){
 String s1="ABCDEFG";  
 byte[] b=s1.getBytes();  
 for(int i=0;i<b.length;i++){  
-System.out.println(barr[i]);  
+System.out.println(b[i]);  
 
 }
 }
